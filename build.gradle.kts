e1b6cb69dbdb2bf17b1045af27390cafdccc46b0
Ubuntu <ubuntu@ip-172-31-75-26.ec2.internal>
2021-02-23 15:49:43 +0000

    change dependency version:

--- build.gradle.kts
+++ build.gradle.kts
@@ -15,7 +15,7 @@ repositories { jcenter() }
 testSets { create("integrationTest") }
 
 dependencies {
-    val flinkVersion = "1.10.1"
+    val flinkVersion = "1.12.1"
     compileOnly("org.apache.flink:flink-java:$flinkVersion")
     compileOnly("org.apache.flink:flink-streaming-java_2.11:$flinkVersion")
 
